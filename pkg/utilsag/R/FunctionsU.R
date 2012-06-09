732593563afc19c68142be07bdef469b7cfe1ef7
agalecki (Andrzej Galecki) <agalecki@umich.edu>
2012-06-09 21:41:23 +0000

    Testing of how to modify namespace in nlme
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/nlmeu@45 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/utilsag/R/FunctionsU.R
@@ -0,0 +1,87 @@
+# .U Functions used to modify nlme namespace
+# Use MODIFY.nlme
+model.matrix.reStruct.U <-
+function (object, data, contrast = NULL, ...) 
+{
+    xverbose(800, "=====> model.matrix.reStruct STARTS")
+    pdDef <- !(length(object) == 1 && inherits(object[[1]],"pdKronecker"))
+
+    if (is.null(form <- formula(object, asList = TRUE))) {
+        stop("Cannot extract model matrix without formula")
+    }
+    form1 <- asOneFormula(form)
+    if (length(form1) > 0) {
+        data <- model.frame(form1, data = data)
+    }
+    else {
+        data <- data.frame("(Intercept)" = rep(1, nrow(data)))
+    }
+###
+    any2list <- function(object, data, contrast) {       # <===
+        form2list <- function(form, data, contrast) {    # <===
+            if (length(asOneFormula(form)) == 0) {
+                return(list("(Intercept)" = rep(1, dim(data)[1])))
+            }
+            as.data.frame(unclass(model.matrix(form, model.frame(form, 
+                data), contrast)))
+        }
+        if (inherits(object, "formula")) {
+            return(form2list(object, data, contrast))
+        }
+        if (is.list(object)) {
+            return(unlist(lapply(object, form2list, data = data, 
+                contrast = contrast), recursive = FALSE))
+        }
+        return(NULL)
+    }
+###
+
+    if (pdDef){     # No pdKroneck
+        value <- as.list(lapply(form, any2list, data = data, contrast = contrast))
+        ncols <- as.vector(unlist(lapply(value, length)))
+        nams <- if (length(value) == 1) {
+        names(value[[1]])
+    }
+    else {
+        paste(rep(names(value), ncols), unlist(lapply(value, 
+            names)), sep = ".")
+    }
+    val <- matrix(unlist(value), nrow = nrow(data), dimnames = list(row.names(data), 
+        nams))
+    attr(val, "nams")  <- as.list(lapply(value, names))
+
+}
+    
+    if (!pdDef){    # pdKroneck
+
+       form2 <- formula(object, asList = FALSE)
+       xverbose(801,form2)
+       form2name <- names(form2) 
+       xverbose(802,form2name)
+       form2 <- form2[[1]]
+       xverbose(803,form2)
+       form  <- form2   ####  <- 
+       ###print(str(data))
+       val <- model.matrix(form,data)
+       ncols <- ncol(val)
+       nams <- dimnames(val)[[2]]
+       xverbose(804,nams)
+       #cat("-> nams \n")
+       #print(nams)
+       namsL <- list(nams)
+       names(namsL) <- form2name 
+       attr(val,"nams") <- namsL
+    }    
+
+    contr <- as.list(lapply(as.data.frame(data), function(x) if (inherits(x, 
+        "factor") && length(levels(x)) > 1) 
+        contrasts(x)
+    else NULL))
+    contr[names(contrast)] <- contrast
+
+    attr(val, "ncols") <- ncols
+    attr(val, "contr") <- contr
+    attr(val,"names")  <- NULL
+    xverbose(800, "=====> model.matrix.reStruct ENDS")
+    val
+}
---
+++ pkg/utilsag/R/nlmeNamespace.R
@@ -0,0 +1,32 @@
+MODIFY1.nlmeNs <- 
+  function(nm, value  
+   ){
+  ## ns: environment class, methods:= as.list(ns), 579 components/functions.
+  ## env:  579 components
+  vnm <- deparse(substitute(value))
+  cat(paste("{", nm, "} <- {", vnm, "}\n", sep=""))
+  ns  <- loadNamespace("nlme")
+  env <-  environment(nlme::nlme)
+  unlockBinding(nm, env)
+  environment(nm) <- environment(value) <- env
+  assign(nm, value, envir=env)
+  assignInNamespace(nm, value, ns=ns, envir = env)    
+  lockBinding(nm, env) 
+ }
+
+.onLoad <- function(libname, pkgname){
+  packageStartupMessage("NOTE: Namespace of the nlme package has been temporarily modified!", appendLF = TRUE)  
+  "To reinstate it use detach(package:nlmeU) command.", appendLF = TRUE)  
+  print(libname)
+  print(pkgname)
+  MODIFY1.nlmeNs("model.matrix.reStruct", utilsag::model.matrix.reStruct.U) # Change utilsag to nlmeU later
+  # MODIFY1.nlmeNs("lmeControl", nlmeU::lmeControl.U)
+  # MODIFY1.nlmeNs("lme.formula", nlmeU::lme.formula.U)
+  # print(as.list(ns)[["model.matrix.reStruct"]])
+  #unloadNamespace("nlme") 
+}
+
+.Last.lib <- function(libpath) {
+  print(libpath)
+  unloadNamespace("nlme")
+}
