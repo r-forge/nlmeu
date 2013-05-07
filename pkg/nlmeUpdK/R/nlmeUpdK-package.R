c18f0ed2253252a22de2a87297a93ed5ba4bf7bf
agalecki (Andrzej Galecki) <agalecki@umich.edu>
2013-05-07 20:13:50 +0000

    pdKronecker  executed using reassignInPackage (R.utils). Documentation needed (use roxygen)
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/nlmeu@88 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/nlmeUpdK/R/nlmeUpdK-package.R
@@ -0,0 +1,32 @@
+#' @importFrom nlme "coef<-" "matrix<-" "Names<-" corMatrix isInitialized logDet Names pdConstruct
+#' @importFrom nlme pdFactor pdMatrix VarCorr
+
+#' @export          "coef<-" "matrix<-" "Names<-" corMatrix isInitialized logDet Names pdConstruct 
+#' @export          pdFactor pdMatrix VarCorr
+
+#' @importFrom nlme Dim asOneFormula getCovariateFormula splitFormula getResponseFormula pdMat
+#' @export          Dim asOneFormula getCovariateFormula splitFormula getResponseFormula pdMat
+
+########
+#' @export pdKronecker
+#' @export model.matrix.reStruct.U
+#' @export unlistFun
+
+###  #S3method "matrix<-", reStruct)
+#' @S3method "["  pdKronecker
+#' @S3method coef  pdKronecker
+#' @S3method "coef<-"  pdKronecker
+#' @S3method corMatrix  pdKronecker
+#' @S3method formula  pdKronecker
+#' @S3method isInitialized  pdKronecker
+#' @S3method logDet  pdKronecker
+#' @S3method "matrix<-"  pdKronecker
+#' @S3method Names  pdKronecker
+#' @S3method "Names<-"  pdKronecker
+#' @S3method pdConstruct  pdKronecker
+#' @S3method pdFactor  pdKronecker
+#' @S3method pdMatrix  pdKronecker
+#' @S3method solve  pdKronecker
+#' @S3method summary  pdKronecker
+#' @S3method VarCorr  pdKronecker
+NULL
